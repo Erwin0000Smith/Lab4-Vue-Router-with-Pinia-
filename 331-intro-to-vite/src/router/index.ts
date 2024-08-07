81bab8a48fb7d6d2115ea71e66fa6e89e0719a60
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-08-08 06:35:09 +0700

    Point 2.1-2.3

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -1,78 +1,100 @@
+// router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
+import StudentListView from '@/views/StudentListView.vue'
+import DetailView from '@/views/event/DetailView.vue'
+import EditView from '@/views/event/EditView.vue'
+import RegisterView from '@/views/event/RegisterView.vue'
+import LayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import nProgress from 'nprogress'
 
-import EventDetailView from '@/views/event/DetailView.vue'
-import EventRegisterView from '@/views/event/RegisterView.vue'
-import EventEditView from '@/views/event/EditView.vue'
-import EventLayoutView from '@/views/event/LayoutView.vue'
-
-import StudentListView from '@/views/StudentListView.vue'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'event-list-view',
-      component: EventListView,
-      props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
-    },
-    {
-      path: '/event/:id',
-      name: 'event-layout-view',
-      component: EventLayoutView,
-      props: true,
-      children: [
-        {
-          path: '',
-          name: 'event-detail-view',
-          component: EventDetailView,
-          props: true
-        },
-        {
-          path: 'register',
-          name: 'event-register-view',
-          component: EventRegisterView,
-          props: true
-        },
-        {
-          path: 'edit',
-          name: 'event-edit-view',
-          component: EventEditView,
-          props: true
-        }
-      ]
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    },
-    {
-      path: '/404/:resource',
-      name: '404-resource-view',
-      component: NotFoundView,
-      props: true
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: 'not-found',
-      component: NotFoundView
-    },
-    {
-      path: '/network-error',
-      name: 'network-error-view',
-      component: NetworkErrorView
-    },
-    {
-      path: '/Student',
-      name: 'student',
-      component: StudentListView
-    }
-  ]
-})
-
-export default router
+export function createAppRouter(pageLimit: (number | null)[]) {
+  const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: '/',
+        name: 'event-list-view',
+        component: EventListView,
+        props: (route) => ({
+          page: parseInt((route.query?.page as string) || '1'),
+          pageLimit
+        })
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: AboutView
+      },
+      {
+        path: '/student',
+        name: 'student',
+        component: StudentListView
+      },
+      {
+        path: '/event/:id',
+        name: 'event-layout-view',
+        component: LayoutView,
+        props: true,
+        children: [
+          {
+            path: '',
+            name: 'event-detail-view',
+            component: DetailView,
+            props: true
+          },
+          {
+            path: 'edit',
+            name: 'event-edit-view',
+            props: true,
+            component: EditView
+          },
+          {
+            path: 'register',
+            name: 'event-register-view',
+            props: true,
+            component: RegisterView
+          }
+        ]
+      },
+      {
+        path: '/event/:id/edit',
+        name: 'event-edit',
+        props: true,
+        component: EditView
+      },
+      {
+        path: '/event/:id/register',
+        name: 'event-register',
+        props: true,
+        component: RegisterView
+      },
+      {
+        path: '/404/:resource',
+        name: '404-resource',
+        component: NotFoundView,
+        props: true
+      },
+      {
+        path: '/:catchAll(.*)',
+        name: 'not-found',
+        component: NotFoundView
+      },
+      {
+        path: '/network-error',
+        name: 'network-error',
+        component: NetworkErrorView
+      }
+    ]
+  })
+  router.beforeEach(() => {
+    nProgress.start()
+  })
+  router.afterEach(() => {
+    nProgress.done()
+  })
+  return router
+}
